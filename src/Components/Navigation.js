0cf6ab05b0e1d01975c1be72e35ffe9951cfee4a
notkalata <kaloyan.vlad.georgiev@gmail.com>
2023-04-18 15:09:55 +0300

    Added active element in Navigation

--- src/Components/Navigation.js
+++ src/Components/Navigation.js
@@ -1,14 +1,27 @@
-import { Link } from "react-router-dom";
+import { Link, useMatch, useResolvedPath } from "react-router-dom";
 
 export default function Navigation(){
     return (
         <div className="Navigation">
             <a href="/"><img src="https://cdn.pixabay.com/photo/2016/11/07/13/04/yoga-1805784__480.png"></img></a>
             <ul>
-                <Link to={'/apartments'}>Apartments</Link>
-                <Link to={'/gallery'}>Gallery</Link>
-                <Link to={'/about'}>About</Link>
+                <CustomLink to="/apartments">Apartments</CustomLink>
+                <CustomLink to="/gallery">Gallery</CustomLink>
+                <CustomLink to="/about">About</CustomLink>
             </ul>
         </div>
     );
 }
+
+function CustomLink({ to, children, ...props }) {
+    const resolvedPath = useResolvedPath(to)
+    const isActive = useMatch({ path: resolvedPath.pathname, end: true })
+  
+    return (
+      <li className={isActive ? "active" : ""}>
+        <Link to={to} {...props}>
+          {children}
+        </Link>
+      </li>
+    )
+  }
