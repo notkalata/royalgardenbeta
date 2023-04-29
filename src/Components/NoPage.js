eb846d43258d145187282b7e5909d6a4c22122e0
notkalata <kaloyan.vlad.georgiev@gmail.com>
2023-04-29 16:46:57 +0300

    minor changes

--- src/Components/NoPage.js
+++ src/Components/NoPage.js
@@ -1,6 +1,9 @@
+import './nopage.css'
+
 export default function NoPage(){
     return(
         <div className="NoPage">
+            <i class="fa-solid fa-triangle-exclamation"></i>
             <h1>Uh uh. We've met a roadblock!</h1>
             <h2>It seems the page you are trying to reach doesn't exist.</h2>
             <p>Error 404: Page doesn't exist.</p>
